d8220fb5b232f56c9910d620f512ce6f13ddff7b
cullen <cullen.dolan@gmail.com>
2021-03-30 10:12:32 -0500

    changed the key structure

--- resource_tracker/settings.py
+++ resource_tracker/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cbrqfp80yi9w)^t)c5#**p*1h529aqp)rtfv8!_xhgtab#2b-g'
+with open('/home/cullen/keys/resource_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
