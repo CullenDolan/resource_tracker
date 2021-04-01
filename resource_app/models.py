ef085560ea8ad5606a919b49f618096a2e89a90b
cullen <cullen.dolan@gmail.com>
2021-03-31 22:07:31 -0500

    connected to the aws db and made the first migration

--- resource_app/models.py
+++ resource_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    # create the choices for the dept
+    PEDIATRICS = 'PEDIATRICS'
+    SURGERY = 'SURGERY'
+    PRIMARYCARE = 'PRIMARY CARE'
+    DEPT_CHOICES = [
+        (PEDIATRICS,'PEDIATRICS'),
+        (SURGERY,'SURGERY'),
+        (PRIMARYCARE,'PRIMARY CARE'),
+    ]
+
+    epic_id = models.IntegerField(primary_key=True, default=0) # NumberInput is the default form input type
+    prov_fname = models.CharField(max_length=50)
+    prov_midinital = models.CharField(max_length=1)
+    prov_lname = models.CharField(max_length=100)
+    prov_division = models.CharField(max_length=200)
+    prov_department = models.CharField(max_length=200, choices=DEPT_CHOICES, default=PEDIATRICS)
+    prov_email = models.EmailField()
+    prov_hospital = models.CharField(max_length=200)
--- resource_tracker/settings.py
+++ resource_tracker/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'resource_app.apps.ResourceAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
