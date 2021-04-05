77600cf798b58a0627067d952df9ca6279c59ade
cullen <cullen.dolan@gmail.com>
2021-04-05 12:47:42 -0500

    added views

--- resource_app/views.py
+++ resource_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 from .models import Provider
 
 
 def home(request):
     all_providers = Provider.objects.order_by('prov_lname')
-    output = ', '.join([p.prov_lname for p in all_providers])
-    return HttpResponse(output)
+    template = loader.get_template('resource_app/index.html')
+    context =  {
+        'all_providers': all_providers
+    }
+    return HttpResponse(template.render(context, request))
 
 def provider_detail(request, epic_id):
     return HttpResponse("You're looking at provider %s" % epic_id)
