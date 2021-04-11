521e1a31e7c05c715243193154b5c96fbea07bc6
cullen <cullen.dolan@gmail.com>
2021-04-10 19:11:59 -0500

    clean commit of the working master

--- resource_app/urls.py
+++ resource_app/urls.py
@@ -8,6 +8,5 @@ app_name = 'resource_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('provider/<int:epic_id>/', views.provider_detail, name='provider_detail'),
-    path('location/<int:id>/', views.location_detail, name='location_detail')
-    #path('<int:epic_id>/add_schedule/', views.add_schedule, name='add_schedule'),
+    path('location/<int:id>/', views.location_detail, name='location_detail'),
 ]
